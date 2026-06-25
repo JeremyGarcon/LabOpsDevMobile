e841e311bfd747423611cce659639e2f352dbd25
Yyrion <ewrondin@gmail.com>
2026-06-25 16:14:03 +0200

    feat(archi-1): screens update

--- services/api.ts
+++ services/api.ts
@@ -2,17 +2,30 @@ import axios from 'axios';
 import { SearchResult, Product } from '../types/searchresult.type';
 import { ProductResponse } from '../types/productreponse.type';
 
-const api = axios.create({
+const offApi = axios.create({
+  baseURL: 'https://world.openfoodfacts.org',
+  timeout: 10000,
+});
+
+const offV2Api = axios.create({
   baseURL: 'https://world.openfoodfacts.org/api/v2/',
   timeout: 10000,
 });
 
-export async function searchProducts(query: string): Promise<SearchResult> {
-  const { data } = await api.get<SearchResult>(`search?${query}`);
+export async function searchProducts(term: string): Promise<SearchResult> {
+  const { data } = await offApi.get<SearchResult>('/cgi/search.pl', {
+    params: {
+      action: 'process',
+      json: true,
+      page_size: 20,
+      fields: 'code,product_name,brands,nutriscore_grade,image_url',
+      ...(term ? { search_terms: term } : {}),
+    },
+  });
   return data;
 }
 
 export async function fetchProduct(code: string): Promise<Product> {
-  const { data } = await api.get<ProductResponse>(`product/${code}`);
+  const { data } = await offV2Api.get<ProductResponse>(`product/${code}`);
   return data.product;
 }
