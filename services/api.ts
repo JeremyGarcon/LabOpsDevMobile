f815fc1b4f73c36224b31cc12f190f2a91c70136
tyfld <tyfld@github.com>
2026-06-25 11:46:10 +0200

    feat(tyfenn):added product info

--- services/api.ts
+++ services/api.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { SearchResult } from '../types/searchresult.type';
+import { Product } from '../types/product.type';
 
 const api = axios.create({
   baseURL: 'https://openfoodfacts.github.io/openfoodfacts-server/api/',
@@ -9,4 +10,9 @@ const api = axios.create({
 export async function searchProducts(query: string): Promise<SearchResult> {
   const { data } = await api.get<SearchResult>(`/search?${query}`);
   return data;
+}
+
+export async function fetchProduct(): Promise<Product[]> {
+ const { data } = await api.get<Product[]>('/produit');
+ return data;
 }
